d6142dd73abf61e4f5f708ebb9a6dd1d543d2d26
gabiplopes04 <gabi.plopes488@gmail.com>
2022-06-29 21:29:50 -0300

    versao inicial

---
+++ script.js
@@ -0,0 +1,48 @@
+const nome          ='Gabriela' 
+const sobrenome     ='Pereira Lopes' 
+const data_nascimento ='20/11/2004'
+let idade = 17
+let altura = 1.64 
+let numero_sapato = 39
+let celular='(18) 98106-6236'
+let cor_favorita = 'Preto'
+
+let  qtde_moto =null
+let  qtde_carro=null
+
+//       <- o valor
+qtde_CARRO= 1
+
+altura =1.76
+
+celular= "(18) 98106-6236"
+
+idade == 26 //
+
+//comparar/validar/verificar igualdade
+
+nome.trim () .toUpperCase() == "GABRIELA" //TRUE
+
+idade == 17 // true 
+
+idade ==18
+
+idade == 25 //true
+
+(true === false) == (true == false)
+
+// igauldade escrita 
+
+nome == "gabriela" //true
+
+nome == ["gabriela"] //false
+
+idade == 17 // true
+
+idade === "26" //false
+
+// Bom dia & cia
+
+
+
+
